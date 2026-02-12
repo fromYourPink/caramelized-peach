a425ff5fe7e9123f6beae1bc987d34d6c03eb9e7
ayeonul <kde013434@gmail.com>
2026-02-12 18:01:06 +0900

    branch init

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,18 @@
 <template>
   <div id="app" class="no-select">
-    <div class="wave-gradient"></div>
-    <div class="dark-veil"></div>
-
-    <transition name="fade" mode="out-in">
-      <div v-if="isNotRoute" class="fade" key="not-route-content">
-        <router-view />
-      </div>
-    </transition>
-
-    <transition name="slide-bounce" mode="out-in">
-      <div
-        v-if="!isNotRoute"
-        class="slide content-container"
-        key="route-content"
-      >
-        <div class="papers">
-          <div class="stacked-paper paper-1"></div>
-          <div class="stacked-paper paper-2"></div>
-        </div>
-        <Header ref="header" />
-        <router-view class="route-view no-scrollbar" :class="{'route-intro':isIntroduce}" />
-        <div class="spacer"></div>
-      </div>
-    </transition>
-
-    <transition name="paper-slide" mode="out-in">
-    <div v-show="!isNotRoute" class="backgroud-papers">
-      <img src="@/assets/imgs/background/신문1.png" class="bg-paper bg-paper-1"/>
-      <img src="@/assets/imgs/background/신문2.png" class="bg-paper bg-paper-2"/>
-      <img src="@/assets/imgs/background/신문3.png" class="bg-paper bg-paper-3"/>
-      <img src="@/assets/imgs/background/신문4.png" class="bg-paper bg-paper-4"/>
-      <img src="@/assets/imgs/background/신문5.png" class="bg-paper bg-paper-5"/>
-      <img src="@/assets/imgs/background/신문6.png" class="bg-paper bg-paper-6"/>
+    <div class="main-bg">
+      <img class="bg-pic pic1" src="@/assets/imgs/background/pic1.png" />
+      <img class="bg-pic pic3" src="@/assets/imgs/background/pic3.png" />
+      <img class="bg-pic pic2" src="@/assets/imgs/background/pic2.png" />
+      <img class="bg-obj obj1" src="@/assets/imgs/background/object1.png" />
+      <img class="bg-obj obj2" src="@/assets/imgs/background/object2.png" />
     </div>
-    </transition>
+
+
+    <div v-if="isRoute" class="fade">
+      <router-view />
+    </div>
+
   </div>
 </template>
 
@@ -48,24 +26,12 @@ export default {
     Footer,
   },
   computed: {
-    isNotRoute() {
-      return this.$route.path == "/";
+    isRoute() {
+      return !(this.$route.path == "/");
     },
-    isIntroduce(){
-      return this.$route.path == "/introduce";
-    }
   },
   data() {
     return {};
-  },
-  mounted() {
-    this.startAnimation();
-  },
-  methods: {
-    startAnimation() {
-      const wave = document.querySelector(".wave-gradient");
-      wave.style.animation = "gradientWave 7s infinite";
-    },
   },
 };
 </script>
@@ -76,74 +42,4 @@ export default {
 </style>
 
 
-<style scoped>
-/* transition */
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.2s ease;
-}
-
-.fade-enter {
-  opacity: 1; /* 나타날 때는 애니메이션 없이 바로 등장 */
-}
-
-.fade-leave-to {
-  opacity: 0; /* 사라질 때는 서서히 투명해짐 */
-}
-
-@keyframes slideInBounce {
-  0% {
-    transform: translateY(100%);
-  }
-  70% {
-    transform: translateY(-8%); /* 살짝 위로 이동 */
-  }
-  100% {
-    transform: translateY(0); /* 제자리로 돌아옴 */
-  }
-}
-
-@keyframes slideOutBounce {
-  0% {
-    transform: translateY(0);
-  }
-  30% {
-    transform: translateY(-8%); /* 위로 살짝 갔다가 */
-  }
-  100% {
-    transform: translateY(100%); /* 아래로 사라짐 */
-  }
-}
-
-
-
-/* slide 트랜지션에 적용되는 바운스 효과 */
-
-.slide-bounce-enter-active, .slide-bounce-leave-active{
-  transition-delay: 0.2s;
-}
-.slide-bounce-enter-active {
-  animation: slideInBounce 0.3s ease-out;
-}
-
-.slide-bounce-leave-active {
-  animation: slideOutBounce 0.3s ease-in;
-}
-
-
-.paper-slide-enter-active, .paper-slide-leave-active {
-  transition: transform 0.2s ease;
-}
-
-.paper-slide-enter {
-  transform: translateY(100%); /* 아래에서 시작 */
-}
-
-.paper-slide-enter-to {
-  transform: translateY(0); /* 위로 올라옴 */
-}
-
-.paper-slide-leave-to {
-  transform: translateY(100%); /* 아래로 내려감 */
-}
-</style>
+<style scoped></style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,23 +2,27 @@
   <!-- if use image, use bg-main-page -->
   <div class="main-page">
     <div class="site-title-section">
-      <div class="site-title">Caramelized</div>
-      <div class="site-title">Peach</div>
+      <img class="site-title" src="@/assets/imgs/title/title.png" />
+
+    </div>
+    <div class="nav-section">
       <div class="main-nav">
-        <router-link to="/introduce" class="main-nav-item"
-          >Introduce</router-link
-        >
-        <router-link to="/gallary" class="main-nav-item">Gallary</router-link>
-        <router-link to="/archive" class="main-nav-item">Archive</router-link>
-        <span
-          class="main-nav-item"
-          @click="
-            openSite(
-              'https://amused-letter-39e.notion.site/Caramelized-Peach-1057274502d0809689d2d2ad0f56400f?pvs=74'
-            )
-          "
-          >Develop-Note<unicon name="link" width="17" height="17" class="icon" fill="#fff"
-        /></span>
+        <router-link to="/introduce" class="main-nav-item">
+          <img class="nav-img" src="@/assets/imgs/icons/folder1.png" />
+          Introduce </router-link>
+        <router-link to="/gallary" class="main-nav-item">
+          <img class="nav-img" src="@/assets/imgs/icons/folder2.png" />Gallary</router-link>
+        <router-link to="/archive" class="main-nav-item"><img class="nav-img"
+            src="@/assets/imgs/icons/folder3.png" />Archive</router-link>
+        <span class="main-nav-item" @click="
+          openSite(
+            'https://amused-letter-39e.notion.site/Caramelized-Peach-1057274502d0809689d2d2ad0f56400f?pvs=74'
+          )
+          ">
+          <img class="nav-img" src="@/assets/imgs/icons/folder4.png" />
+          Develop-Note
+          <unicon name="link" width="17" height="17" class="icon" fill="#fff" />
+        </span>
       </div>
     </div>
   </div>
@@ -26,14 +30,8 @@
 
 <script>
 export default {
-  mounted() {
-    this.startAnimation();
-  },
   methods: {
-    startAnimation() {
-      const wave = document.querySelector(".wave-gradient");
-      wave.style.animation = "gradientWave 7s infinite";
-    },
+
     openSite(url) {
       window.open(url, "_blank");
     },
