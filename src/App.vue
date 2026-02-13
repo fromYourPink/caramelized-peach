3428cf02420d79df9261f563dae522173b964e8b
ayeonul <kde013434@gmail.com>
2026-02-13 11:17:46 +0900

    배경화면을 route에서 분리:

--- src/App.vue
+++ src/App.vue
@@ -8,27 +8,29 @@
       <img class="bg-obj obj2" src="@/assets/imgs/background/object2.png" />
     </div>
 
-    <div class="site-title-section">
-      <img class="site-title" src="@/assets/imgs/title/title.png" />
-    </div>
-    <div class="nav-section">
-      <div class="main-nav">
-        <router-link to="/introduce" class="main-nav-item">
-          <img class="nav-img" src="@/assets/imgs/icons/folder1.png" />
-          Introduce </router-link>
-        <router-link to="/gallary" class="main-nav-item">
-          <img class="nav-img" src="@/assets/imgs/icons/folder2.png" />Gallary</router-link>
-        <router-link to="/archive" class="main-nav-item"><img class="nav-img"
-            src="@/assets/imgs/icons/folder3.png" />Archive</router-link>
-        <span class="main-nav-item" @click="
-          $openSite(
-            'https://amused-letter-39e.notion.site/Caramelized-Peach-1057274502d0809689d2d2ad0f56400f?pvs=74'
-          )
-          ">
-          <img class="nav-img" src="@/assets/imgs/icons/folder4.png" />
-          Develop-Note
-          <unicon name="link" width="17" height="17" class="icon" fill="#fff" />
-        </span>
+    <div class="iconarea-container">
+      <div class="site-title-section">
+        <img class="site-title" src="@/assets/imgs/title/title.png" />
+      </div>
+      <div class="nav-section">
+        <div class="main-nav">
+          <router-link to="/introduce" class="main-nav-item">
+            <img class="nav-img" src="@/assets/imgs/icons/folder1.png" />
+            Introduce </router-link>
+          <router-link to="/gallary" class="main-nav-item">
+            <img class="nav-img" src="@/assets/imgs/icons/folder2.png" />Gallary</router-link>
+          <router-link to="/archive" class="main-nav-item"><img class="nav-img"
+              src="@/assets/imgs/icons/folder3.png" />Archive</router-link>
+          <span class="main-nav-item" @click="
+            $openSite(
+              'https://amused-letter-39e.notion.site/Caramelized-Peach-1057274502d0809689d2d2ad0f56400f?pvs=74'
+            )
+            ">
+            <img class="nav-img" src="@/assets/imgs/icons/folder4.png" />
+            Develop-<br>Note
+            <!-- <unicon name="link" width="13" height="13" class="icon" fill="#fff" /> -->
+          </span>
+        </div>
       </div>
     </div>
     <div v-if="isRoute" class="fade">
